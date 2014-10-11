ee9bde34e3bd7c4c557f8db128b0ba8969981cbf
sherwin <whereaswhile@gmail.com>
2014-10-11 16:33:47 -0500

    add rotation perturbation in ListaPrvd_image.py

--- common/ListaPrvd_image.py
+++ common/ListaPrvd_image.py
@@ -13,7 +13,7 @@ from w_util import readLines
 
 class ListaSet:
     def __init__(self, paramfile):
-        self.param = {'paramfile': paramfile}
+        self.param = {'paramfile': paramfile, 'rotate': 1}
         plines=readLines(paramfile)
         for l in plines:
             l=l.rstrip().split()
@@ -22,6 +22,7 @@ class ListaSet:
         self.param['outsize']=int(self.param['outsize'])
         self.param['mrgsize']=(self.param['inpsize']-self.param['outsize'])/2
         self.param['smplside']=int(self.param['smplPerSide'])
+        self.param['rotate']=max(1, min(4, int(self.param['rotate'])))
 # 	print self.param
         MAT_IN_VAR=self.param['MAT_IN_VAR']
         MAT_OUT_VAR=self.param['MAT_OUT_VAR']
@@ -32,7 +33,7 @@ class ListaSet:
         self.Z=d[MAT_OUT_VAR]
         assert(len(np.shape(self.X))==2)
         self.imgnum=self.X.shape[1]
-        self.pertnum=self.param['smplside']**2
+        self.pertnum=(self.param['smplside']**2)*self.param['rotate']
         self.datanum=self.imgnum*self.pertnum
         self.indim=self.param['inpsize']**2 #np.prod(np.shape(self.X[0,0]))
         self.outdim=self.param['outsize']**2 #np.prod(np.shape(self.Z[0,0]))
@@ -40,7 +41,7 @@ class ListaSet:
         self.output=[self.Z[0, i] for i in range(self.imgnum)]
         if self.param['train']=='0':
             self.output=[_*0 for _ in self.output]
-        print '%d samples found' % self.datanum
+        print '%d images, %d samples found' % (self.imgnum, self.datanum)
 
     def get_num_images(self):
         return self.datanum
@@ -54,18 +55,36 @@ class ListaSet:
     def get_input(self, idx):
         img_id=idx/self.pertnum
         pert_id=idx%self.pertnum
+        rot_id=pert_id%self.param['rotate']
+        off_id=pert_id/self.param['rotate']
         [h, w]=self.input[img_id].shape
-        [dy, dx]=self.get_offset(h, w, pert_id)
-        return self.input[img_id][dy:dy+self.param['inpsize'], dx:dx+self.param['inpsize']]
+        [dy, dx]=self.get_offset(h, w, off_id)
+        res=self.input[img_id][dy:dy+self.param['inpsize'], dx:dx+self.param['inpsize']]
+        if rot_id==1:
+            res=np.fliplr(res)
+        elif rot_id==2:
+            res=np.flipud(res)
+        elif rot_id==3:
+            res=res.T
+        return res
 
     def get_output(self, idx):
         img_id=idx/self.pertnum
         pert_id=idx%self.pertnum
+        rot_id=pert_id%self.param['rotate']
+        off_id=pert_id/self.param['rotate']
         [h, w]=self.output[img_id].shape
-        [dy, dx]=self.get_offset(h, w, pert_id)
+        [dy, dx]=self.get_offset(h, w, off_id)
         dy+=self.param['mrgsize']
         dx+=self.param['mrgsize']
-        return self.output[img_id][dy:dy+self.param['outsize'], dx:dx+self.param['outsize']]
+        res=self.output[img_id][dy:dy+self.param['outsize'], dx:dx+self.param['outsize']]
+        if rot_id==1:
+            res=np.fliplr(res)
+        elif rot_id==2:
+            res=np.flipud(res)
+        elif rot_id==3:
+            res=res.T
+        return res
     
     def getmeta(self, idx):
         return self.param
@@ -84,7 +103,7 @@ def getStore(param):
 def test(param):
     ts = ListaSet(param)
     print "{} images in total".format(ts.get_num_images())
-    for i in range(150728,150928,20):
+    for i in range(728,748,1):
         im=ts.get_input(i)
         y=ts.get_output(i)
         print "i={}, input={},\toutput={}".format(i, im.shape, y.shape)
